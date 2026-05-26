84480dc2b3f57932d60d5b0958686a7995b4da8d
Franco Herrera <franncoherrera2011@gmail.com>
2026-05-25 23:55:01 -0300

    chore: update searchbox style

--- src/components/ui/top-menu/TopMenu.tsx
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -1,10 +1,10 @@
-'use client';
-import { MiniCart } from '@/components/cart/minicart/MiniCart';
-import { titleFont } from '@/config/fonts';
-import { useCartStore, useUIStore } from '@/store';
-import Link from 'next/link';
-import { useEffect, useState } from 'react';
-import { IoCartOutline, IoSearchOutline } from 'react-icons/io5';
+"use client";
+import { MiniCart } from "@/components/cart/minicart/MiniCart";
+import { titleFont } from "@/config/fonts";
+import { useCartStore, useUIStore } from "@/store";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { IoCartOutline, IoSearchOutline } from "react-icons/io5";
 
 export const TopMenu = () => {
   const openSideMenu = useUIStore((state) => state.openSideMenu);
@@ -18,7 +18,7 @@ export const TopMenu = () => {
   return (
     <nav className="flex px-5 justify-between items-center w-full ">
       <div>
-        <Link href={'/'}>
+        <Link href={"/"}>
           <span className={`${titleFont.className} antialiased font-bold`}>
             Clothes
           </span>
@@ -29,31 +29,32 @@ export const TopMenu = () => {
       <div className="hidden sm:block">
         <Link
           className="m-2 p-2 rounded-md transition-all hover:bg-gray-100"
-          href={'/gender/men'}
+          href={"/gender/men"}
         >
           Hombres
         </Link>
         <Link
           className="m-2 p-2 rounded-md transition-all hover:bg-gray-100"
-          href={'/gender/women'}
+          href={"/gender/women"}
         >
           Mujeres
         </Link>
         <Link
           className="m-2 p-2 rounded-md transition-all hover:bg-gray-100"
-          href={'/gender/kid'}
+          href={"/gender/kid"}
         >
           Niños
         </Link>
       </div>
 
       <div className="flex items-center">
-        <Link href="/search" className="mx-1">
-          <IoSearchOutline className="w-5 h-5" />
-        </Link>
+        <div className="m-2">
+          <IoSearchOutline className=" w-5 h-5" />
+        </div>
+
         <div className="group">
           <Link
-            href={totalItemsInCart === 0 && loaded ? '/empty' : '/cart'}
+            href={totalItemsInCart === 0 && loaded ? "/empty" : "/cart"}
             className="mx-2"
           >
             <div className="relative">
